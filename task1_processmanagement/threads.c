da7e76eab7c930d510ba1d0d59989df97b8691a6
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-02 10:43:40 +0000

    shared variable and synchronization

--- task1_processmanagement/threads.c
+++ task1_processmanagement/threads.c
@@ -3,6 +3,12 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <stdlib.h>
+#define THREADS 3
+#define QUANTUM 2
+
+int *shared;
+pthread_mutex_t lock;
+sem_t sem;
 int main() {
     printf("Process Management Assignment\n");
     return 0;
