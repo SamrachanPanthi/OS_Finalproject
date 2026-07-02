4cfef542eea2694b6ed478ece7d084d7c4e5aab9
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-02 10:40:52 +0000

    added all the required import

--- task1_processmanagement/threads.c
+++ task1_processmanagement/threads.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <stdlib.h>
 int main() {
     printf("Process Management Assignment\n");
     return 0;
