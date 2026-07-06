95def91cb043a1aec194b4c23b9222663c059458
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-06 14:14:27 +0000

    implemented round robin

--- task1_processmanagement/threads.c
+++ task1_processmanagement/threads.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#define QUANTUM 2
 
 pthread_mutex_t lock;
 sem_t sem;
@@ -65,7 +66,47 @@ void *billingThread(void *arg)
     sem_post(&sem);
     return NULL;
 }
+void roundRobin()
+{
+    int burst[3]={5,3,4};
 
+    char *process[3]={
+        "Order",
+        "Kitchen",
+        "Billing"
+    };
+
+    int completed=0;
+
+    printf("\n========== Round Robin ==========\n");
+
+    while(completed<3)
+    {
+        for(int i=0;i<3;i++)
+        {
+            if(burst[i]>0)
+            {
+                if(burst[i]>QUANTUM)
+                {
+                    printf("%s executes for %d units\n",
+                            process[i],QUANTUM);
+
+                    burst[i]-=QUANTUM;
+                }
+                else
+                {
+                    printf("%s completed.\n",process[i]);
+
+                    burst[i]=0;
+
+                    completed++;
+                }
+
+                sleep(1);
+            }
+        }
+    }
+}
 int main()
 {
     pthread_t t1, t2, t3;
@@ -85,7 +126,8 @@ int main()
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
     pthread_join(t3, NULL);
-
+    
+    roundRobin();
     printf("\nCompleted Orders = %d\n", *completedOrders);
 
     pthread_mutex_destroy(&lock);
