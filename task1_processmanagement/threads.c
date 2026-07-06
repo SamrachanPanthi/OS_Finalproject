72cccc3b2a92578d1691248c10d379e8cdb75219
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-06 14:55:45 +0000

    implemented deadlock

--- task1_processmanagement/threads.c
+++ task1_processmanagement/threads.c
@@ -9,7 +9,11 @@ pthread_mutex_t lock;
 sem_t sem;
 
 int *completedOrders;
+//deadlock prevention REsource
 
+
+pthread_mutex_t resource1;
+pthread_mutex_t resource2;
 void *orderThread(void *arg)
 {
     sem_wait(&sem);
@@ -106,17 +110,39 @@ void roundRobin()
             }
         }
     }
+
+}
+void *deadlockDemo(void *arg)
+{
+    const char *name = (const char *)arg;
+
+    pthread_mutex_lock(&resource1);
+    printf("%s locked Resource 1\n", name);
+
+    sleep(1);
+
+    pthread_mutex_lock(&resource2);
+    printf("%s locked Resource 2\n", name);
+
+    printf("%s completed successfully\n", name);
+
+    pthread_mutex_unlock(&resource2);
+    pthread_mutex_unlock(&resource1);
+
+    return NULL;
 }
 int main()
 {
     pthread_t t1, t2, t3;
+    pthread_t d1,d2;
 
     completedOrders = (int *)malloc(sizeof(int));
     *completedOrders = 0;
 
     pthread_mutex_init(&lock, NULL);
     sem_init(&sem, 0, 2);
-
+    pthread_mutex_init(&resource1,NULL);
+    pthread_mutex_init(&resource2,NULL);
     printf("Restaurant Order Management System\n\n");
 
     pthread_create(&t1, NULL, orderThread, NULL);
@@ -128,11 +154,19 @@ int main()
     pthread_join(t3, NULL);
     
     roundRobin();
+    // Deadlock Prevention Demo
+    pthread_create(&d1, NULL, deadlockDemo, "Manager");
+    pthread_create(&d2, NULL, deadlockDemo, "Supervisor");
+
+    pthread_join(d1, NULL);
+    pthread_join(d2, NULL);
     printf("\nCompleted Orders = %d\n", *completedOrders);
 
     pthread_mutex_destroy(&lock);
     sem_destroy(&sem);
-
+    pthread_mutex_destroy(&resource1);
+    pthread_mutex_destroy(&resource2);
+    printf("\nTotal Completed Orders = %d\n",*completedOrders);
     free(completedOrders);
 
     return 0;
