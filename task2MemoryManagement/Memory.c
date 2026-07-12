c28b0c4208801f7a3faf5476a6e65eef84246607
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-12 14:10:25 +0000

    add paging and frame display function

--- task2MemoryManagement/Memory.c
+++ task2MemoryManagement/Memory.c
@@ -8,7 +8,20 @@ int videoPages[TOTAL_REQUESTS] =
 {
     7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2
 };
-
+void displayFrames(int frames[])
+{
+    for (int i = 0; i < FRAME_COUNT; i++)
+    {
+        if (frames[i] == -1)
+        {
+            printf("- ");
+        }
+        else
+        {
+            printf("%d ", frames[i]);
+        }
+    }
+}
 int main()
 {
     printf(" VIDEO STREAMING MEMORY SIMULATOR\n");
