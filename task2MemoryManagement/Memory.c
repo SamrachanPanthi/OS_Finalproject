a1d0673442d37dab24ce006a11f8372d809685d4
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-12 14:02:31 +0000

    memory management simulator

--- task2MemoryManagement/Memory.c
+++ task2MemoryManagement/Memory.c
@@ -1,1 +1,19 @@
-//MEMORY Management
+#include <stdio.h>
+
+#define FRAME_COUNT 3
+#define PAGE_SIZE 4
+#define TOTAL_REQUESTS 13
+
+int videoPages[TOTAL_REQUESTS] =
+{
+    7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2
+};
+
+int main()
+{
+    printf(" VIDEO STREAMING MEMORY SIMULATOR\n");
+    printf("Page Size : %d KB\n", PAGE_SIZE);
+    printf("Frames    : %d\n", FRAME_COUNT);
+
+    return 0;
+}
