81e971701b5636c84f8b055b2744db65c3e63de5
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-20 16:43:05 +0000

    Add client-server communication and message exchange

--- Task4_Network/Client.c
+++ Task4_Network/Client.c
@@ -1,55 +1,39 @@
+/*
+    GameCore Multiplayer Client
+*/
+
 #include <stdio.h>
-#include <stdlib.h> 
-#include <unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #define SERVER_PORT 8080
-#include <string.h>
+#define BUFFER_SIZE 256
 
-char username[50];
-char password[50];
-char loginInfo[100];
-char reply[100];
-
-printf("\nUsername: ");
-scanf("%49s", username);
-
-printf("Password: ");
-scanf("%49s", password);
-
-sprintf(
-    loginInfo,
-    "%s:%s",
-    username,
-    password
-);
-
-send(
-    socketFD,
-    loginInfo,
-    strlen(loginInfo) + 1,
-    0
-);
-
-recv(
-    socketFD,
-    reply,
-    sizeof(reply) - 1,
-    0
-);
-
-printf("\nServer: %s\n", reply);
 int main()
 {
     int socketFD;
 
     struct sockaddr_in serverAddress;
 
-    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    char username[50];
+    char password[50];
+
+    char loginInfo[100];
+    char reply[BUFFER_SIZE];
+    char message[BUFFER_SIZE];
+
+    socketFD = socket(
+        AF_INET,
+        SOCK_STREAM,
+        0
+    );
 
     if (socketFD < 0)
     {
         printf("Socket creation failed.\n");
+
         return 1;
     }
 
@@ -57,19 +41,79 @@ int main()
     serverAddress.sin_port = htons(SERVER_PORT);
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(
+    if (
+        connect(
             socketFD,
             (struct sockaddr *)&serverAddress,
-            sizeof(serverAddress)) < 0)
+            sizeof(serverAddress)
+        ) < 0
+    )
     {
-        printf("Connection failed.\n");
+        printf("Unable to connect.\n");
 
         close(socketFD);
 
         return 1;
     }
 
-    printf("Connected to GameCore server.\n");
+    printf("\n===== GAMECORE CLIENT =====\n");
+
+    printf("Username: ");
+    scanf("%49s", username);
+
+    printf("Password: ");
+    scanf("%49s", password);
+
+    sprintf(
+        loginInfo,
+        "%s:%s",
+        username,
+        password
+    );
+
+    send(
+        socketFD,
+        loginInfo,
+        strlen(loginInfo) + 1,
+        0
+    );
+
+    recv(
+        socketFD,
+        reply,
+        sizeof(reply) - 1,
+        0
+    );
+
+    printf("\nServer: %s\n", reply);
+
+    getchar();
+
+    printf("\nEnter message: ");
+
+    fgets(
+        message,
+        sizeof(message),
+        stdin
+    );
+
+    message[strcspn(message, "\n")] = '\0';
+
+    send(
+        socketFD,
+        message,
+        strlen(message) + 1,
+        0
+    );
+
+    recv(
+        socketFD,
+        reply,
+        sizeof(reply) - 1,
+        0
+    );
+
+    printf("Server Response: %s\n", reply);
 
     close(socketFD);
 
