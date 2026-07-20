43433b66abbfbbb1aded3e4cff9ba755cbf1a1be
SamrachanaPanthi <panthisamrachana@gmail.com>
2026-07-20 16:39:12 +0000

    Implement player authentication and login validation

--- Task4_Network/Client.c
+++ Task4_Network/Client.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdlib.h> 
+#include <unistd.h> 
 #include <arpa/inet.h>
 
 #define SERVER_PORT 8080
+#include <string.h>
 
+char username[50];
+char password[50];
+char loginInfo[100];
+char reply[100];
+
+printf("\nUsername: ");
+scanf("%49s", username);
+
+printf("Password: ");
+scanf("%49s", password);
+
+sprintf(
+    loginInfo,
+    "%s:%s",
+    username,
+    password
+);
+
+send(
+    socketFD,
+    loginInfo,
+    strlen(loginInfo) + 1,
+    0
+);
+
+recv(
+    socketFD,
+    reply,
+    sizeof(reply) - 1,
+    0
+);
+
+printf("\nServer: %s\n", reply);
 int main()
 {
     int socketFD;
